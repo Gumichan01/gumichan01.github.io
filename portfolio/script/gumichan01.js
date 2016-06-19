eed2b4e3cf209e1d8e9ce76cf351572abf8b4a52
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-06-19 16:33:34 +0200

    Added the assets used by the website

---
+++ portfolio/script/gumichan01.js
@@ -0,0 +1,16 @@
+
+
+var date_maj="23 mai 2016";
+var date_maj_en="23th may, 2016";
+
+
+function maj_date_fr()
+{
+	document.getElementById("maj").innerHTML = date_maj;
+}
+
+
+function maj_date_en()
+{
+	document.getElementById("maj").innerHTML = date_maj_en;
+}
