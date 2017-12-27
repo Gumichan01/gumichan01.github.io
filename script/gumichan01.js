3203abbc282265765e6638b141a3575c0812ea94
Gumichan01 <luxon.jean.pierre@gmail.com>
2017-12-27 11:04:14 +0100

    css/script: new directories

---
+++ script/gumichan01.js
@@ -0,0 +1,16 @@
+
+
+var date_maj="20 décembre 2017";
+var date_maj_en="20th December, 2017";
+
+
+function maj_date_fr()
+{
+	document.getElementById("maj").innerHTML = date_maj;
+}
+
+
+function maj_date_en()
+{
+	document.getElementById("maj").innerHTML = date_maj_en;
+}
